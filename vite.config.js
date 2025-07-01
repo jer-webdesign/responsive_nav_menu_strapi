f00deff317adc663746ec29ab35a5a51b545273c
Jerry <jeremy.g.olanda@gmail.com>
2025-06-30 18:28:43 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/responsive_nav_menu_strapi/',
+  base: '/',
+  // base: '/responsive_nav_menu_strapi/',
   server: {
     proxy: {
       '/api': {
