48304122adfa3c90fe39e1a6658e8609a00e0616
Jerry <jeremy.g.olanda@gmail.com>
2025-06-30 18:11:33 -0600

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@
 
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import './NavBar.css';
+import './Navbar.css';
 
 import visBookLogo from '/assets/images/vblogo.png';
 import booksIcon from '/assets/images/icons/books-icon.png';
